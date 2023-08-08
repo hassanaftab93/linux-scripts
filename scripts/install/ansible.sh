ff873eb355ec27edcbba1903547d0149815d97e6
hassan_aftab <teo.devops@teo-intl.com>
2023-08-08 14:22:20 +0500

    Add ansible script

---
+++ scripts/install/ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update apt and install dependencies
+sudo apt update
+sudo apt install software-properties-common
+
+# Add repository
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+
+# Install Ansible
+sudo apt install ansible
+ansible --version
