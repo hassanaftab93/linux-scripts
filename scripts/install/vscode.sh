d782e2cab927b3ece7a52bc7c4be0c5949190a91
hassan_aftab <teo.devops@teo-intl.com>
2023-08-18 15:49:11 +0500

    Added vscode.sh

---
+++ scripts/install/vscode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update apt and install dependencies
+sudo apt update
+sudo apt install software-properties-common apt-transport-https
+
+# Download the repository and import Microsoft’s GPG key as shown:
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Update and Install vs code
+sudo apt update
+sudo apt install code
