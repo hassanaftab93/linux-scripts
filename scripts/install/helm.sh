ad805916fb6430624504d58fde2bc832aeb0ef20
Hassan.Aftab <hassanaftab.pk@gmail.com>
2023-08-21 10:58:14 +0500

    Added helm.sh

---
+++ scripts/install/helm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install curl
+sudo apt update
+sudo apt install -y curl
+
+# Curl helm keys
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+
+# Install apt-transport-http
+sudo apt install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+# Install helm
+sudo apt update
+sudo apt install helm
