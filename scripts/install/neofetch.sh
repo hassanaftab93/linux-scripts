765ce6a03cde48e4acd463434ed29052aa251315
Hassan.Aftab <hassanaftab.pk@gmail.com>
2023-08-24 17:07:11 +0500

    Added neofetch.sh

---
+++ scripts/install/neofetch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://www.geeksforgeeks.org/tree-command-unixlinux/
+
+# Update apt and install dependencies
+sudo apt update
+
+# Install neofetch
+sudo apt-get install -y neofetch
