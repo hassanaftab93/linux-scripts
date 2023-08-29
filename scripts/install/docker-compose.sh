61620b9f3dfdeb05c0162718d7069b09d5353c9a
Hassan.Aftab <hassanaftab.pk@gmail.com>
2023-08-29 10:32:56 +0500

    Added docker compose

---
+++ scripts/install/docker-compose.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update apt and install dependencies
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add repo
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update apt and install dependencies
+sudo apt update
+
+# Install Docker-Compose
+sudo apt install -y docker-compose
+
+# Verify version
+docker-compose -v
