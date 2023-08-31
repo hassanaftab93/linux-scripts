cfafbb4cae728e41f8d30cdcfd4f9b176dc86663
Hassan.Aftab <hassanaftab.pk@gmail.com>
2023-08-31 22:55:09 +0500

    Added ulauncher.sh

---
+++ scripts/install/ulauncher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update apt and install dependencies
+sudo apt update
+
+# Install gnupg keys
+sudo apt install -y gnupg
+gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
+gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
+          http://ppa.launchpad.net/agornostal/ulauncher/ubuntu jammy main" \
+          | sudo tee /etc/apt/sources.list.d/ulauncher-jammy.list
+
+
+sudo apt update
+sudo apt install ulauncher
+ulauncher -v
