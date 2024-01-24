386da90665d368738eca14558ea392544c500a1e
Hassan.Aftab <hassanaftab.pk@gmail.com>
2024-01-24 11:57:39 +0500

    Add Portainer

---
+++ scripts/install/portainer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Update apt and install dependencies
+sudo apt update
+
+sudo docker pull portainer/portainer-ce:latest
+sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
