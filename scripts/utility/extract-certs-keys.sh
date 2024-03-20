977d4df16d06371467010af761f90fb4f3ce2155
Hassan.Aftab <hassanaftab.pk@gmail.com>
2024-03-20 16:28:11 +0500

    Add dynamic path

--- scripts/utility/extract-certs-keys.sh
+++ scripts/utility/extract-certs-keys.sh
@@ -1,19 +1,20 @@
-#/#!/bin/bash
+#!/bin/bash
 
 # Pass in filename for certificate and textfile for password e.g
-# ./extract-certs-keys.sh certificate.pfx password.txt
+
+# ./extract-certs-keys.sh path_to_certificate.pfx path_to_password_file.txt
 
 # Assign values to variables
 certificate_file="$1"
 password_file="$2"
 password=$(cat "$password_file")
 
+cert_dir=$(dirname "${certificate_file}")
+outputs_folder="${cert_dir}/outputs"
+mkdir -p "$outputs_folder"
+
 # Generate certificate
-openssl pkcs12 -in "$certificate_file" -passin pass:"$password" -passout pass:"$password" -nokeys | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > my-cert.crt
+openssl pkcs12 -in "$certificate_file" -passin pass:"$password" -passout pass:"$password" -nokeys | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > "$outputs_folder/my-cert.crt"
 
 # Generate private key
-openssl pkcs12 -in "$certificate_file" -nocerts -passin pass:"$password" -passout pass:"$password" | openssl rsa -passin pass:"$password" -passout pass:"$password" -out my-key.key
-
-mkdir -p outputs
-mv my-cert.crt outputs/
-mv my-key.key outputs/
+openssl pkcs12 -in "$certificate_file" -nocerts -passin pass:"$password" -passout pass:"$password" | openssl rsa -passin pass:"$password" -passout pass:"$password" -out "$outputs_folder/my-key.key"
