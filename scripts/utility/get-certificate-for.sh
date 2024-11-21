1ccd6a1ab2c5dcf48187649d8f0446814909d67a
Hassan Aftab <hassanaftab.pk@gmail.com>
2024-11-21 22:10:28 +0500

    Make updates

--- scripts/utility/get-certificate-for.sh
+++ scripts/utility/get-certificate-for.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 domain_name=$1
+wildcard=$2
+dirName="$domain_name-certificate"
+
+if [ "$wildcard" = "true" ]; then
+  cert_domain_name="*.$domain_name"
+else
+  cert_domain_name=$domain_name
+fi
+
+mkdir -p $dirName/miscFiles
 
 # Generate certificate signing request
-openssl req -new -newkey rsa:2048 -nodes -keyout $domain_name.key -out $domain_name.csr
+openssl req -new -newkey rsa:2048 -nodes -keyout ./$dirName/miscFiles/$cert_domain_name.key -out ./$dirName/miscFiles/$cert_domain_name.csr
 
 # Generate self-signed certificate
-sudo certbot certonly --manual --preferred-challenges=dns -d $domain_name
+sudo certbot certonly --manual --preferred-challenges=dns -d $cert_domain_name
 
 sleep 5
 
 # Copy cert and key
-sudo cp /etc/letsencrypt/live/$domain_name/{fullchain.pem,privkey.pem} .
+sudo cp /etc/letsencrypt/live/${cert_domain_name#\*.}/{fullchain.pem,privkey.pem} ./$dirName
