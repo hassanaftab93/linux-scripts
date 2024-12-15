2d5b07c222070d6eb13d087bad56064571cb19f6
Hassan Aftab <hassanaftab.pk@gmail.com>
2024-12-15 08:12:41 -0800

    Update terragrunt sign

--- scripts/install/terragrunt.sh
+++ scripts/install/terragrunt.sh
@@ -3,6 +3,6 @@
 # Install curl
 sudo apt update
 cd ~/Downloads/
-curl -o terragrunt -L https://github.com/gruntwork-io/terragrunt/releases/download/v0.55.1/terragrunt_linux_amd64
+curl -o terragrunt -L https://github.com/gruntwork-io/terragrunt/releases/download/v0.69.13/terragrunt_linux_amd64
 chmod u+x terragrunt
 sudo mv terragrunt /usr/local/bin/terragrunt
