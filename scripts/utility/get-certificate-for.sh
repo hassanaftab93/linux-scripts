48887895ce4b673b9a08041ac65b44adc0fdd814
Hassan Aftab <hassanaftab.pk@gmail.com>
2025-02-25 16:56:26 +0500

    CSR not required

--- scripts/utility/get-certificate-for.sh
+++ scripts/utility/get-certificate-for.sh
@@ -5,8 +5,8 @@ dirName="$domain_name-certificate"
 
 mkdir -p $dirName/miscFiles
 
-# Generate certificate signing request (not needed for Certbot, but keeping for reference)
-openssl req -new -newkey rsa:2048 -nodes -keyout ./$dirName/miscFiles/$domain_name.key -out ./$dirName/miscFiles/$domain_name.csr
+# # Generate certificate signing request (not needed for Certbot, but keeping for reference)
+# openssl req -new -newkey rsa:2048 -nodes -keyout ./$dirName/miscFiles/$domain_name.key -out ./$dirName/miscFiles/$domain_name.csr
 
 # Request both root and wildcard certificates
 sudo certbot certonly --manual --preferred-challenges=dns -d "$domain_name" -d "*.$domain_name"
