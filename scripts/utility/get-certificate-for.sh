cd85fdf92d6d84f2f035e3ac1476a6353c5b642e
Hassan Aftab <hassanaftab.pk@gmail.com>
2025-02-25 17:29:27 +0500

    Updated script for handling certs once cert is created

--- scripts/utility/get-certificate-for.sh
+++ scripts/utility/get-certificate-for.sh
@@ -4,7 +4,8 @@ domain_name=$1
 dirName="$domain_name-certificate"
 
 # Create directory for certificate files
-mkdir -p $dirName
+certPath="$HOME/Downloads/certificates-from-certbot/$dirName"
+mkdir -p $certPath
 
 # Install Certbot and Simply.com DNS Plugin (if not installed)
 if ! command -v certbot &>/dev/null; then
@@ -12,17 +13,18 @@ if ! command -v certbot &>/dev/null; then
     sudo apt update && sudo apt install -y certbot
 fi
 
-# # Generate certificate signing request (not needed for Certbot, but keeping for reference)
-# openssl req -new -newkey rsa:2048 -nodes -keyout ./$dirName/miscFiles/$domain_name.key -out ./$dirName/miscFiles/$domain_name.csr
-
 # Request both root and wildcard certificates
 sudo certbot certonly --manual --preferred-challenges=dns -d "$domain_name" -d "*.$domain_name"
 
 sleep 5
 
 # Copy certificate and key
-sudo cp /etc/letsencrypt/live/$domain_name/{fullchain.pem,privkey.pem} ./$dirName
+sudo cp /etc/letsencrypt/live/$domain_name/{fullchain.pem,privkey.pem} $certPath
+
+sudo rm -rf /etc/letsencrypt/live/$domain_name
+sudo rm -rf /etc/letsencrypt/archive/$domain_name
+sudo rm -rf /etc/letsencrypt/renewal/$domain_name.conf
 
 # Concatenate key and cert into a single PEM file
-sudo cat ./$dirName/privkey.pem ./$dirName/fullchain.pem > ./$dirName/$domain_name.pem
-sudo rm -rf ./$dirName/privkey.pem ./$dirName/fullchain.pem
+sudo cat $certPath/privkey.pem $certPath/fullchain.pem > $certPath/$domain_name.pem
+# sudo rm -rf ./$dirName/privkey.pem ./$dirName/fullchain.pem
